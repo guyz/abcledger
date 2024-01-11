5452d3f20e9769c4da53249314d06152db0df87f
Guy Zyskind <guy@zyskind.com>
2024-01-11 21:12:49 +0200

    Debug  PRinter

---
+++ DebugPrint.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Guy Zyskind on 11/01/2024.
+//
+
+#include "DebugPrint.h"
+
+DebugPrinter debugPrint;
---
+++ DebugPrint.h
@@ -0,0 +1,34 @@
+//
+// Created by Guy Zyskind on 11/01/2024.
+//
+
+#ifndef DPFPIR_DEBUGPRINT_H
+#define DPFPIR_DEBUGPRINT_H
+
+#include <iostream>
+
+// Define a macro for enabling or disabling debug output
+// Uncomment the below line to enable debug output
+// #define ENABLE_DEBUG
+
+class DebugPrinter {
+public:
+    template<typename T>
+    DebugPrinter& operator<<(const T& value) {
+#ifdef ENABLE_DEBUG
+        std::cout << value;
+#endif
+        return *this;
+    }
+
+    DebugPrinter& operator<<(std::ostream& (*pf)(std::ostream&)) {
+#ifdef ENABLE_DEBUG
+        std::cout << pf;
+#endif
+        return *this;
+    }
+};
+
+extern DebugPrinter debugPrint;
+
+#endif // DEBUG_PRINT_H
